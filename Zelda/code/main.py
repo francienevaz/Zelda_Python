0a169af46efcce603b7da78543512377ea44af75
francienevaz <fran.vfraga@gmail.com>
2023-11-19 18:31:09 -0300

    updated

--- Zelda/code/main.py
+++ Zelda/code/main.py
@@ -1,6 +1,5 @@
 import pygame, sys
 from settings import *
-from debug import debug
 
 class Game:
 	def __init__(self):
@@ -17,7 +16,6 @@ class Game:
 					pygame.quit()
 					sys.exit()
 			self.screen.fill('black')
-			debug('hello :)')
 			pygame.display.update()
 			self.clock.tick(FPS)
 
