3f465d7a265a13718053f710775619e8dbf9bd6d
francienevaz <fran.vfraga@gmail.com>
2025-03-04 20:41:10 -0300

    creating camera

--- Zelda/code/level.py
+++ Zelda/code/level.py
@@ -10,7 +10,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
 
         # sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
         # sprite setup
@@ -28,6 +28,27 @@ class Level:
     
     def run(self):
         # update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+        #debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        # general setup
+        super().__init__()        
+        self.display_surface = pygame.display.get_surface()
+        # dessa forma e possivel passar parametros para o Vector2 e mudar a posicao da tela do jogo
+        # mas nao mudamos a posicao do jogo em si, apenas desenhamos todos os elementos em um lugar diferente
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2(100, 200)
+
+    def custom_draw(self, player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery -self.half_height
+
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)  
