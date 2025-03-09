04ef785aa6a7ee3af11052a281379be7a5664a6f
francienevaz <fran.vfraga@gmail.com>
2025-03-09 01:46:42 -0300

    update - ground

---
+++ Zelda/levels/tilesets/ground.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ground" tilewidth="64" tileheight="64" tilecount="2850" columns="57">
+ <image source="../../graphics/tilemap/ground.png" width="3648" height="3200"/>
+</tileset>
