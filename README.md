fa55486cb179b712fac74a2f1c1eb0f5e54254c9
francienevaz <fran.vfraga@gmail.com>
2025-03-16 01:09:45 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Zelda_Python
+
+For running venv, entry into Zelda's folder:
+ ```bash
+ source venv/bin/activate
+ ```
+
+ 
--- Zelda/code/level.py
+++ Zelda/code/level.py
@@ -3,6 +3,8 @@ from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from random import choice
 
 class Level:
     def __init__(self):
@@ -17,15 +19,36 @@ class Level:
         self.create_map()
     
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x, y), [self.visible_sprites,self.obstacles_sprites])
-                elif col == 'p':
-                    self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
+        layouts = {
+            'boundary': import_csv_layout('./map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('./map/map_Grass.csv'),
+            'object': import_csv_layout('./map/map_Objects.csv'),
+        }
+        graphics = {
+            'grass': import_folder('./graphics/grass'),
+            'object': import_folder('./graphics/objects'),
+        }
     
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x, y),  [self.obstacles_sprites], 'invisible')
+                        if style == 'grass':
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x,y), [self.visible_sprites, self.obstacles_sprites], 'grass', random_grass_image)
+                            
+                        if style == 'object':
+                            surf = graphics['object'][int(col)]  
+                            Tile((x,y), [self.visible_sprites, self.obstacles_sprites], 'object', surf)
+        #         if col == 'x':
+        #             Tile((x, y), [self.visible_sprites,self.obstacles_sprites])
+        #         elif col == 'p':
+        #             self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacles_sprites)
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
