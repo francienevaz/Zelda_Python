9b2d478461932e1a03979a9f7e0e7547fa9b2369
francienevaz <fran.vfraga@gmail.com>
2025-03-23 15:16:29 -0300

    creating weapons

--- Zelda/code/level.py
+++ Zelda/code/level.py
@@ -16,6 +16,9 @@ class Level:
         self.visible_sprites = YSortCameraGroup()
         self.obstacles_sprites = pygame.sprite.Group()
 
+        # attack sprites
+        self.current_attack = None
+
         # sprite setup
         self.create_map()
     
@@ -49,11 +52,16 @@ class Level:
         #             Tile((x, y), [self.visible_sprites,self.obstacles_sprites])
         #         elif col == 'p':
         #             self.player = Player((x, y), [self.visible_sprites], self.obstacles_sprites)
-        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacles_sprites, self.create_attack)
+        self.player = Player((2000, 1430), [self.visible_sprites], self.obstacles_sprites, self.create_attack, self.destroy_weapon)
  
     def create_attack(self):
-        Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
  
+    def destroy_weapon(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None    
+
     def run(self):
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
--- Zelda/code/player.py
+++ Zelda/code/player.py
@@ -3,7 +3,7 @@ from settings import *
 from support import import_folder
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos, groups, obstacles_sprites, create_attack):
+    def __init__(self, pos, groups, obstacles_sprites, create_attack, destroy_weapon):
         super().__init__(groups)
         self.image = pygame.image.load('./graphics/test/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
@@ -22,9 +22,18 @@ class Player(pygame.sprite.Sprite):
         self.attacking = False
         self.attack_cooldown = 400
         self.attack_time = None
-        self.create_attack = create_attack
 
         self.obstacles_sprites = obstacles_sprites
+
+        # weapon
+        self.create_attack = create_attack
+        self.destroy_weapon = destroy_weapon
+        self.weapon_index = 0
+        self.weapon = list(weapon_data.keys())[self.weapon_index]
+        self.can_switch_weapon = True
+        self.weapon_switch_time = None
+        self.switch_duration_cooldown = 200
+
 
     def import_player_assets(self):
         character_path = './graphics/player/'
@@ -71,6 +80,15 @@ class Player(pygame.sprite.Sprite):
                 self.attacking = True  
                 self.attack_time = pygame.time.get_ticks()
                 print('magic')
+
+            if keys[pygame.K_q] and self.can_switch_weapon:
+                self.can_switch_weapon = False
+                self.weapon_switch_time = pygame.time.get_ticks()
+                if self.weapon_index < len(list(weapon_data.keys())) - 1:
+                    self.weapon_index += 1  
+                else:
+                    self.weapon_index = 0    
+                self.weapon = list(weapon_data.keys())[self.weapon_index]  
         
     def get_status(self):
         # idle status
@@ -123,6 +141,11 @@ class Player(pygame.sprite.Sprite):
         if self.attacking:
             if current_time - self.attack_time >= self.attack_cooldown:
                 self.attacking = False
+                self.destroy_weapon()
+
+        if not self.can_switch_weapon:
+            if current_time - self.weapon_switch_time >= self.switch_duration_cooldown:
+                self.can_switch_weapon = True        
 
     def animate(self):
         animation = self.animations[self.status]
